5ecc59e5cb15ee7b804542ed59d1a0d724eeeeaa
Michael-IRL <iszekeell@gmail.com>
2017-12-03 22:26:17 +0000

    first commit four states of jsPromises

---
+++ jsPromises.js
@@ -0,0 +1,7 @@
+/*
+ * javaScript promise have four states 
+ * Fulfilled (Resolved): Operation was successful
+ * Rejected : Opertion failed
+ * Pending : Opertin is sill waiting to finish
+ * Settled : Somthing happened
+ */
